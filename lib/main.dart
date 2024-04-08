bdb38e3f14bcd2af9c54ab14a04545e49c712630
Ilie Dumitru <iliemity@yahoo.com>
2024-04-08 20:57:33 +0300

    Andrei; Basic Look, DB conn, Basic add game integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,289 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(GameAdapter());
+  await Hive.openBox<Game>('games');
+
+  runApp(MaterialApp(
+    title: 'MediaMaster',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Game {
+  int id;
+  String name;
+  String backgroundImage;
 
-  // This widget is the root of your application.
+  Game({required this.id, required this.name, required this.backgroundImage});
+}
+
+class GameAdapter extends TypeAdapter<Game> {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Media Master',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  final int typeId = 0;
+
+  @override
+  Game read(BinaryReader reader) {
+    return Game(
+      id: reader.readInt(),
+      name: reader.readString(),
+      backgroundImage: reader.readString(),
     );
+  }
+
+  @override
+  void write(BinaryWriter writer, Game obj) {
+    writer.writeInt(obj.id);
+    writer.writeString(obj.name);
+    writer.writeString(obj.backgroundImage);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyApp extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyAppState extends State<MyApp> {
+  late Box<Game> gameBox;
+  int selectedGameIndex = 0;
+
+  // Placeholder image URL
+  static const String placeholderImageUrl =
+      'https://uncensoredtactical.com/wp-content/uploads/2021/04/Placeholder-1920x1080-1.jpg';
+
+  @override
+  void initState() {
+    super.initState();
+    gameBox = Hive.box<Game>('games');
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('MediaMaster'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Media Master Demo',
+      body: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[200],
+              child: ValueListenableBuilder(
+                valueListenable: gameBox.listenable(),
+                builder: (context, Box<Game> box, _) {
+                  return ListView.builder(
+                    itemCount: box.length,
+                    itemBuilder: (context, index) {
+                      final game = box.getAt(index)!;
+                      return ListTile(
+                        title: Text(game.name),
+                        onTap: () {
+                          setState(() {
+                            selectedGameIndex = index;
+                          });
+                        },
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            _showDeleteConfirmationDialog(context, index);
+                          },
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      gameBox.isNotEmpty ? gameBox.getAt(selectedGameIndex)!.backgroundImage : placeholderImageUrl
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                color: Colors.black.withOpacity(0.5),
+                child: Center(
+                  child: Text(
+                    gameBox.isNotEmpty ? gameBox.getAt(selectedGameIndex)!.name : '',
+                    style: TextStyle(color: Colors.white, fontSize: 24.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showSearchGameDialog(context);
+        },
+        tooltip: 'Search Game',
+        child: Icon(Icons.search),
+      ),
+    );
+  }
+
+  Future<void> _showSearchGameDialog(BuildContext context) async {
+    TextEditingController searchController = TextEditingController();
+    List<dynamic> searchResults = [];
+
+    bool noSearch = true; // Flag to track if there are no search results
+
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: Text('Search for a Game'),
+              content: Container(
+                height: noSearch ? 100 : 150, // Set height based on the presence of search results
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        labelText: 'Game Name',
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () async {
+                            String query = searchController.text;
+                            if (query.isNotEmpty) {
+                              searchResults = await _searchGame(query);
+                              setState(() {
+                                noSearch = searchResults.isEmpty; // Update noSearch flag
+                              }); // Trigger rebuild to show results and update flag
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    if (searchResults.isNotEmpty)
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              SizedBox(height: 2),
+                              ...searchResults.map((result) {
+                                String gameName = result['title'];
+                                return ListTile(
+                                  title: Text(gameName),
+                                  onTap: () {
+                                    _addGame(gameName);
+                                    Navigator.of(context).pop();
+                                  },
+                                );
+                              }).toList(),
+                            ],
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<List<dynamic>> _searchGame(String query) async {
+    final String url =
+        'https://www.pcgamingwiki.com/w/api.php?action=query&format=json&list=search&srsearch=$query';
+
+    try {
+      final http.Response response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        return data['query']['search'];
+      } else {
+        throw Exception('Failed to search for game: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error searching game: $error');
+      return [];
+    }
+  }
+
+  Future<void> _showSearchResultsDialog(BuildContext context, List<dynamic> searchResults) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Select a Game'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: searchResults.map((result) {
+                String gameName = result['title'];
+                return ListTile(
+                  title: Text(gameName),
+                  onTap: () {
+                    _addGame(gameName);
+                    Navigator.of(context).pop();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _addGame(String gameName) async {
+    Game newGame = Game(
+      id: DateTime.now().millisecondsSinceEpoch,
+      name: gameName,
+      backgroundImage: placeholderImageUrl,
+    );
+    gameBox.add(newGame);
+  }
+
+  Future<void> _showDeleteConfirmationDialog(BuildContext context, int index) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete Game'),
+          content: Text('Are you sure you want to delete this game?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                gameBox.deleteAt(index);
+                setState(() {
+                  selectedGameIndex = 0; // Move to the first game
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Delete'),
             ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
