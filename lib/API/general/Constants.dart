e9ed5ca25ab6b3b87ad01e0fd7852f4cb1003428
Neculae Andrei-Fabian <67969123+Andrei137@users.noreply.github.com>
2025-10-03 21:13:29 +0300

    small changes
    Removed sensitive information from constants.

--- lib/API/general/Constants.dart
+++ lib/API/general/Constants.dart
@@ -1,6 +1,6 @@
-const String clientIdIGDB = "f8j10ype0p8dqo280l58dspeko0sma";
-const String clientSecretIGDB = "adqcli8ijxm5fkhji0yn7ed0mvt81h";
+const String clientIdIGDB = "";
+const String clientSecretIGDB = "";
 const String accessTokenTMDB =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2RmZDY2NjdiNDc3M2UwYzc1OWUzNWRlMzg3ZjhmZCIsInN1YiI6IjY2MTY0MGRhY2U1ZDgyMDE0OThmOTg1ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z4scRXtpzGIh0W9qhGB8zNMFabmW1Ub3aPaRRY99ijg";
+    "";
 const String userAgentsGoodreads =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
+    "";
